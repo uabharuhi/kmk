b448080b6dd6a5d60e9390dca2d7f13489163da3
uabharuhi <uabharuhi@gmail.com>
2017-07-07 18:41:01 +0800

    visulize activations by tensorboard, keras callback approach give up

--- model.py
+++ model.py
@@ -1,13 +1,12 @@
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D#, MaxPooling2D
-from keras.utils import plot_model
 import keras
 import data
 
-batch_size = 16
 num_classes = 3
-epochs = 20
+
+#label shape !!! : n*1 or n
 X,y  = data.read_dataset_from_pkl()
 X_train,X_val,X_test = X['train'].astype('float32')/255,X['val'].astype('float32')/255,X['test'].astype('float32')/255
 y_train,y_val,y_test = y['train'],y['val'],y['test']
@@ -15,50 +14,50 @@ y_train,y_val,y_test = y['train'],y['val'],y['test']
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_val = keras.utils.to_categorical(y_val, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-#label shape !!! : n*1 or n
 
 print('x_train shape:', X_train.shape)
-print(y_train)
+#print(y_train)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
+def simple_model(batch_size,epochs):
+	model = Sequential()
+	model.add(Conv2D(16, (3, 3), padding='same',data_format='channels_last',input_shape=X_train.shape[1:]))
+	# if not add input shape The first layer in a Sequential model must get an `input_shape` or `batch_input_shape` argument.
+	model.add(Activation('relu',name='conv1_a') )
 
+	#model.add(Conv2D(16, (3, 3), padding='same',data_format='channels_last'))#default is channels_last
+	#model.add(Activation('relu'))
+	#model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
+	model.add(Flatten())
+	#model.add(Dense(50))
+	model.add(Dense(10))
+	model.add(Activation('relu',name='dense1_a') )
+	model.add(Dense(num_classes))
+	model.add(Activation('softmax') )
 
-model = Sequential()
-model.add(Conv2D(16, (3, 3), padding='same',data_format='channels_last',input_shape=X_train.shape[1:]))
-# if not add input shape The first layer in a Sequential model must get an `input_shape` or `batch_input_shape` argument.
-model.add(Activation('relu',name='conv1_a') )
+	return model
 
-#model.add(Conv2D(16, (3, 3), padding='same',data_format='channels_last'))#default is channels_last
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+## defined callback
+class ActivationLogger(keras.callbacks.Callback):
+	activation_history = {}
+	X_batch = None
 
-model.add(Flatten())
-#model.add(Dense(50))
-model.add(Dense(10))
-model.add(Activation('relu',name='dense1_a') )
-model.add(Dense(num_classes))
-model.add(Activation('softmax') )
+	def __init__(self,X_batch):
+		self.X_batch = X_batch
 
-#opt = keras.optimizers.Adadelta()
-opt = keras.optimizers.SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+	def on_train_begin(self, logs={}):
+		self.losses = []
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+	def on_epoch_end(self, epoch, logs={}):
 
-hist = model.fit(X_train, y_train,
-		batch_size=batch_size,
-		epochs=epochs,
-		validation_data=(X_val, y_val),
-		shuffle=True
-		,callbacks=[keras.callbacks.ModelCheckpoint('./save/best_model.hdf5'),keras.callbacks.History()])
-print(hist.history)
-model.save('./save/final_model.hdf5')  # creates a HDF5 file 'my_model.h5'
-
-plot_model(model, to_file='./model.png')
-
-del model
-
+		import show_activation
+		#print('== ==')
+		if 'conv1_a' not in self.activation_history:
+			self.activation_history['conv1_a'] = []
+		#print('batch shape')
+		print('epoch:%d'%epoch)
+		a = show_activation.get_layer_output(self,'conv1_a',self.X_batch)
+		self.activation_history['conv1_a'].append(a)
--- show_activation.py
+++ show_activation.py
@@ -11,86 +11,22 @@
 
 #model.layers[0].input
 #model.get_layer
-from keras.models import load_model
-import data
 from keras import backend as K
-import matplotlib.pyplot as plt
 
-batch_size = 16
-num_classes = 3
+#mode 0  test 1 training
+#get sequetial model  output of a layer...
+#self a model instance = keras call back self
 
-X,y  = data.read_dataset_from_pkl()
-X_train,X_val,X_test = X['train'].astype('float32')/255,X['val'].astype('float32')/255,X['test'].astype('float32')/255
-y_train,y_val,y_test = y['train'],y['val'],y['test']
+def get_layer_output(self,name,epoch_X):
+	                     # tensorflow tensor
+	print('shape of epoch_X')
+	print(epoch_X.shape)
+	# dont know how to  get traning activation ... so use test mode
+	output  = K.function([self.model.layers[0].input,K.learning_phase()],
+		                                                        #numpy array
+                         [self.model.get_layer(name).output] )( [epoch_X,0])[0]
 
-model = load_model('./save/final_model.hdf5')
-
-exp_test = X_test[0:batch_size]
-y_pred = model.predict(exp_test, batch_size=batch_size)
-
-conv_a = K.function([model.layers[0].input,K.learning_phase()], # if use dropout must use learning phase and set  mode
-                                  [model.get_layer('conv1_a').output])([exp_test,0])[0]
-
-f1_a = K.function([model.layers[0].input,K.learning_phase()],
-                                  [model.get_layer('dense1_a').output ])([exp_test,0])[0]
-
-
-
-
-print('type of  model.layers[0].input')
-print(type(model.layers[0].input))
-
-print('type of  model.get_layer(conv1_a).output')
-print(type(model.get_layer('conv1_a').output)) # by this way we can get tensorflow tensor
-
-
-
-print('type of conv1')
-print(type(conv_a))
-
-print('shape of conv activation')
-print(conv_a.shape)
-print('shape of fl_a')
-print(f1_a.shape)
-
-print('conv activation')
-#print(conv_a)
-print('- - '*50)
-print('f1')
-print(f1_a)
-
-#show by matplot
-#conv_a of first picture
-#(16, 128, 128, 16)
-#def show_conva_mean_std(conv_a,shape):
-#	f, axarr = plt.subplots(shape[0],shape[1])
-#	for i,ax in enumerate(axarr):
-#		i1 = i//4 #integer divide
-#		i2 = i%4
-#		axarr[i1,i2].imshow(conv_a[:,:,i], cmap=plt.get_cmap('gray'))
-def show_conva(pic_index,conv_a):
-	f, axarr = plt.subplots(4,4)
-	conv_a = conv_a[pic_index]
-	for i,ax in enumerate(axarr):
-		i1 = i//4 #integer divide
-		i2 = i%4
-		axarr[i1,i2].imshow(conv_a[:,:,i], cmap = plt.get_cmap('gray'),vmin=0.0, vmax=1.0)
-	f.savefig('conv%d.jpg'%pic_index)
-
-show_conva(0,conv_a)
-show_conva(1,conv_a)
-#dense
-#16*10
-#f, axarr = plt.subplots(4,4)
-#for i,ax in enumerate(axarr):
-	#i1 = i/5
-	#i2 = i%5
-	#axarr[i1,i2].imshow(f1_a[i], cmap=plt.get_cmap('gray'))
-
-#dense
-#plt.imshow(bg, cmap=plt.get_cmap('gray'), vmin=0, vmax=255)
-
-
+	return output # numpy array
 
 
 
